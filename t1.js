8f103ff2e37bfc07b5c6739a55a81477000afc3a
xiaoming1120 <486366762@qq.com>
2020-09-30 13:42:08 +0800

    添加文件

---
+++ t1.js
@@ -0,0 +1,19 @@
+function student (stuschool , stuclass , stunumber , stuage)
+    {
+        this.studentschool = stuschool;
+        this.studentclass  = stuclass;
+        this.studentnumber = stunumber;
+        this.studentage    = stuage;
+        this.fristname     = "quan"
+    }
+let liu = new student("11" , "1701" , "123456" , "20");
+let lin = new student("12" , "1702" ,  "654321" , "21");
+let a   =123456.123;
+var st  = "strnigname" ;
+var tn  = st[5];
+var sl  = st.length;
+var ln  =typeof(sl);
+
+console.log( typeof(student) , a.toExponential() , lin  , tn , typeof(tn) , ln ,typeof(x));
+console.log(liu.max);
+
